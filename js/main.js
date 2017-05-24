484bb35854d5155906ce7417e0f3a826f6a970e0
Nikita Sokol <snn0389@gmail.com>
2017-05-24 14:14:52 +0700

    download2

--- js/main.js
+++ js/main.js
@@ -1,98 +1,16 @@
 $(document).ready(function () {
 
-	// Find all YouTube videos
-var $allVideos = $("iframe[src^='https://www.youtube.com']"),
+  // Hamburger button
 
-    // The element that is fluid width
-    $fluidEl = $(".video");
+  var $toggleButton = $('.toggle-button'),
+      $menuWrap = $('.menu-wrap'),
+      $sidebarArrow = $('.menu-sidebar .sidebar-menu-arrow');
+        //$menuParentItem = $('.menu-sidebar .menu-item-has-children');
 
-// Figure out and save aspect ratio for each video
-$allVideos.each(function() {
-
-  $(this)
-    .data('aspectRatio', this.height / this.width)
-
-    // and remove the hard coded width/height
-    .removeAttr('height')
-    .removeAttr('width');
-
-});
-
-// When the window is resized
-$(window).resize(function() {
-
-  var newWidth = $fluidEl.width();
-
-  // Resize all videos according to their own aspect ratio
-  $allVideos.each(function() {
-
-    var $el = $(this);
-    $el
-      .width(newWidth)
-      .height(newWidth * $el.data('aspectRatio'));
-
+  $toggleButton.on('click', function() {
+    $(this).toggleClass('button-open');
+    $menuWrap.toggleClass('menu-show');
   });
-
-// Kick off one resize to fix all videos on page load
-}).resize();
-
-    
-// Slider_02 - Swiper
-    var swiper = new Swiper('.swiper1', {
-        pagination: '.swiper-pagination',
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
-        paginationClickable: true,
-        spaceBetween: 0,
-        autoplay: 2500,
-        autoplayDisableOnInteraction: false,
-        slidesPerView: 'auto',
-        centeredSlides: true,
-        loop: true
-    });
-
-    var swiper2 = new Swiper('.swiper2', {
-        pagination: '.swiper-pagination',
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
-        paginationClickable: true,
-        spaceBetween: 100,
-        autoplay: 2500,
-        setWrapperSize: true,
-        autoplayDisableOnInteraction: false,
-        slidesPerView: '1',
-        centeredSlides: true,
-        loop: true
-    });
-
-    var swiper3 = new Swiper('.swiper3', {
-        pagination: '.swiper-pagination',
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
-        paginationClickable: false,
-        spaceBetween: 0,
-        autoplay: 2500,
-        autoplayDisableOnInteraction: false,
-        slidesPerView: 1,
-        centeredSlides: true,
-        effect: 'slide',
-        loop: true
-    });
-
-    var swiper4 = new Swiper('.swiper4', {
-        pagination: '.swiper-pagination',
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
-        paginationClickable: false,
-        spaceBetween: 0,
-        autoplay: 2500,
-        autoplayDisableOnInteraction: false,
-        slidesPerView: 1,
-        centeredSlides: true,
-        effect: 'slide',
-        loop: true
-    });
-
 
 //fancybox
   $(".fancybox").fancybox({
@@ -100,24 +18,9 @@ $(window).resize(function() {
    'buttons':false
   });
 
-  // anchors
-   $('.anchor').click(function () {
-  		var hrefName = $(this).attr('href');
-		$('html, body').animate({
-	        scrollTop: $(hrefName).offset().top - 40
-	    }, 600);
-	});
+  //mask for phone
+   $(".form-phone").mask("+7 (999) 999-9999");
 
-   $(".collapsed-anchor").click(function () {
-   		$('.collapsed-menu').fadeOut(300);
-   })
 
-   $('.menu-button').click(function(){
-		$('.collapsed-menu').fadeIn(300);
-	});
 
-	$('.close-button').click(function () {
-		$('.collapsed-menu').fadeOut(300);
-	});
-
-})
+});
